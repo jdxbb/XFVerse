8dfae6f0470b9b1291cdc172f6b368724b4858ac
jdxbb <jdxbb@users.noreply.github.com>
2026-05-27 04:05:01 +0800

    7.0c完成

--- src/MediaLibrary.App/Views/MainWindow.xaml.cs
+++ src/MediaLibrary.App/Views/MainWindow.xaml.cs
@@ -1,4 +1,10 @@
+using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
 using MediaLibrary.App.Services;
 using MediaLibrary.App.ViewModels.Main;
 
@@ -6,9 +12,232 @@ namespace MediaLibrary.App.Views;
 
 public partial class MainWindow : Window
 {
+    private const int WmGetMinMaxInfo = 0x0024;
+    private const int MonitorDefaultToNearest = 0x00000002;
+
+    private HwndSource? _hwndSource;
+
     public MainWindow()
     {
         InitializeComponent();
         DataContext = AppServiceProvider.GetRequiredService<MainWindowViewModel>();
+        SourceInitialized += OnSourceInitialized;
+        StateChanged += OnWindowStateChanged;
+        UpdateMaximizeRestoreButton();
+    }
+
+    protected override void OnClosed(EventArgs e)
+    {
+        _hwndSource?.RemoveHook(WindowProc);
+        SourceInitialized -= OnSourceInitialized;
+        StateChanged -= OnWindowStateChanged;
+        base.OnClosed(e);
+    }
+
+    private void OnSourceInitialized(object? sender, EventArgs e)
+    {
+        _hwndSource = (HwndSource?)PresentationSource.FromVisual(this);
+        _hwndSource?.AddHook(WindowProc);
+    }
+
+    private void OnWindowStateChanged(object? sender, EventArgs e)
+    {
+        UpdateMaximizeRestoreButton();
+    }
+
+    private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState.Minimized;
+    }
+
+    private void MaximizeRestoreButton_Click(object sender, RoutedEventArgs e)
+    {
+        ToggleMaximizeRestore();
+    }
+
+    private void CloseButton_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ChangedButton != MouseButton.Left || IsInteractiveElement(e.OriginalSource as DependencyObject))
+        {
+            return;
+        }
+
+        if (e.ClickCount == 2)
+        {
+            ToggleMaximizeRestore();
+            e.Handled = true;
+            return;
+        }
+
+        BeginWindowDrag(e);
+    }
+
+    private void BeginWindowDrag(MouseButtonEventArgs e)
+    {
+        if (WindowState == WindowState.Maximized)
+        {
+            RestoreWindowForDrag(e);
+        }
+
+        try
+        {
+            DragMove();
+        }
+        catch (InvalidOperationException)
+        {
+            // DragMove requires the left mouse button to remain pressed.
+        }
+    }
+
+    private void RestoreWindowForDrag(MouseButtonEventArgs e)
+    {
+        var restoreBounds = RestoreBounds;
+        var mousePosition = PointToScreen(e.GetPosition(this));
+        var source = PresentationSource.FromVisual(this);
+
+        if (source?.CompositionTarget is not null)
+        {
+            mousePosition = source.CompositionTarget.TransformFromDevice.Transform(mousePosition);
+        }
+
+        WindowState = WindowState.Normal;
+        Left = mousePosition.X - (restoreBounds.Width * 0.5);
+        Top = Math.Max(SystemParameters.VirtualScreenTop, mousePosition.Y - 12);
+    }
+
+    private void ToggleMaximizeRestore()
+    {
+        WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+    }
+
+    private void UpdateMaximizeRestoreButton()
+    {
+        if (MaximizeRestoreButton is null)
+        {
+            return;
+        }
+
+        if (WindowState == WindowState.Maximized)
+        {
+            MaximizeRestoreButton.Content = "❐";
+            MaximizeRestoreButton.ToolTip = "还原";
+            return;
+        }
+
+        MaximizeRestoreButton.Content = "□";
+        MaximizeRestoreButton.ToolTip = "最大化";
+    }
+
+    private static bool IsInteractiveElement(DependencyObject? source)
+    {
+        while (source is not null)
+        {
+            if (source is ButtonBase
+                or TextBoxBase
+                or PasswordBox
+                or Selector
+                or MenuItem
+                or Thumb
+                or Slider
+                or ScrollBar)
+            {
+                return true;
+            }
+
+            source = VisualTreeHelper.GetParent(source) ?? LogicalTreeHelper.GetParent(source);
+        }
+
+        return false;
+    }
+
+    private nint WindowProc(nint hwnd, int msg, nint wParam, nint lParam, ref bool handled)
+    {
+        if (msg == WmGetMinMaxInfo)
+        {
+            ApplyMonitorWorkArea(hwnd, lParam);
+            handled = true;
+        }
+
+        return nint.Zero;
+    }
+
+    private void ApplyMonitorWorkArea(nint hwnd, nint lParam)
+    {
+        var monitor = MonitorFromWindow(hwnd, MonitorDefaultToNearest);
+        if (monitor == nint.Zero)
+        {
+            return;
+        }
+
+        var monitorInfo = new MonitorInfo
+        {
+            Size = Marshal.SizeOf<MonitorInfo>()
+        };
+
+        if (!GetMonitorInfo(monitor, ref monitorInfo))
+        {
+            return;
+        }
+
+        var minMaxInfo = Marshal.PtrToStructure<MinMaxInfo>(lParam);
+        var workArea = monitorInfo.WorkArea;
+        var monitorArea = monitorInfo.MonitorArea;
+
+        minMaxInfo.MaxPosition.X = Math.Abs(workArea.Left - monitorArea.Left);
+        minMaxInfo.MaxPosition.Y = Math.Abs(workArea.Top - monitorArea.Top);
+        minMaxInfo.MaxSize.X = Math.Abs(workArea.Right - workArea.Left);
+        minMaxInfo.MaxSize.Y = Math.Abs(workArea.Bottom - workArea.Top);
+
+        var dpi = VisualTreeHelper.GetDpi(this);
+        minMaxInfo.MinTrackSize.X = (int)Math.Ceiling(MinWidth * dpi.DpiScaleX);
+        minMaxInfo.MinTrackSize.Y = (int)Math.Ceiling(MinHeight * dpi.DpiScaleY);
+
+        Marshal.StructureToPtr(minMaxInfo, lParam, true);
+    }
+
+    [DllImport("user32.dll")]
+    private static extern nint MonitorFromWindow(nint hwnd, int flags);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    private static extern bool GetMonitorInfo(nint monitor, ref MonitorInfo monitorInfo);
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct PointInfo
+    {
+        public int X;
+        public int Y;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct MinMaxInfo
+    {
+        public PointInfo Reserved;
+        public PointInfo MaxSize;
+        public PointInfo MaxPosition;
+        public PointInfo MinTrackSize;
+        public PointInfo MaxTrackSize;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct RectInfo
+    {
+        public int Left;
+        public int Top;
+        public int Right;
+        public int Bottom;
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+    private struct MonitorInfo
+    {
+        public int Size;
+        public RectInfo MonitorArea;
+        public RectInfo WorkArea;
+        public int Flags;
     }
 }
